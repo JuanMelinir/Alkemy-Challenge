6e935ad14e39e7060259f3f3bfcb38a38b28db28
JuanMelinir <juan.melinir@est.fi.uncoma.edu.ar>
2022-02-03 21:43:34 -0300

    securityconfiguration

--- src/main/java/com/alkemy/challenge/challenge/auth/config/SecurityConfiguration.java
+++ src/main/java/com/alkemy/challenge/challenge/auth/config/SecurityConfiguration.java
@@ -4,16 +4,19 @@ import com.alkemy.challenge.challenge.auth.service.UserDetailsCustomService;
 import com.alkemy.challenge.challenge.filter.JwtResquestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -27,7 +30,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
-    return NoOpPasswordEncoder.getInstance();
+    //return NoOpPasswordEncoder.getInstance();
+    return new BCryptPasswordEncoder();
     }
     @Override
     @Bean
--- src/main/java/com/alkemy/challenge/challenge/auth/service/JwtUtils.java
+++ src/main/java/com/alkemy/challenge/challenge/auth/service/JwtUtils.java
@@ -37,8 +37,9 @@ public interface JwtUtils {
     }
 
     private String createToken(Map<String,Object>claims, String subject){
-    return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+        int expirationInHours=10;
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * expirationInHours ))
             .signWith(SignatureAlgorithm.HS256,SECRET_KEY).compact();
     }
     public Boolean validateToken(String token,UserDetails userDetails){
